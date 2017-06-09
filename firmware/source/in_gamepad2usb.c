ed6bb8759b1115e4e0a21d6f097af535f4234c43
mleonid2000 <mleonid2000@gmail.com>
2017-06-09 23:11:09 +0300

    Create in_gamepad2usb.c

--- firmware/source/in_gamepad2usb.c
+++ firmware/source/in_gamepad2usb.c
@@ -48,11 +48,11 @@
 
 //=============================================================================
 // Global Variables
-t_gamepad2usb gamepad2usb_data[1];			//Buttons data
+t_gamepad2usb gamepad2usb_data[1];		//Buttons data
 t_gamepad2usb gamepad2usb_data_turbo[1];	//Autofire Buttons data
 
 static unsigned short int holdoncounter = 0;//Autofire activate button counter
-static unsigned char autofire_flag = 0;		//Autofire flag
+static unsigned char autofire_flag = 0;	//Autofire flag
 unsigned char autofire_counter = 0;	//Autofire pattern 2on:2off after USB send
 
 //=============================================================================
@@ -62,23 +62,23 @@ void inDecoderInit(void)
 	//All pullups
 	PORTB	 =	0x1F;
 	DDRB	 =	( 0 << PB0 ) | ( 0 << PB1 ) | ( 0 << PB2 ) |
-				( 0 << PB3 ) | ( 0 << PB4 ) | ( 0 << PB5 ) ;
+			( 0 << PB3 ) | ( 0 << PB4 ) | ( 0 << PB5 ) ;
 	
 	//All pullups
 	PORTC	 =	0x1F;
 	DDRC	 =	( 0 << PC0 ) | ( 0 << PC1 ) | ( 0 << PC2 ) |
-				( 0 << PC3 ) | ( 0 << PC4 ) | ( 0 << PC5 ) ;
+			( 0 << PC3 ) | ( 0 << PC4 ) | ( 0 << PC5 ) ;
 
 	//All pullups
 	PORTD	|=	0xFA;
 	DDRD	|=	( 0 << PD0 ) | ( 0 << PD1 ) | ( 0 << PD2 ) |
-				( 0 << PD3 ) | ( 0 << PD4 ) | ( 0 << PD5 ) |
-				( 0 << PD6 ) | ( 0 << PD7 );
+			( 0 << PD3 ) | ( 0 << PD4 ) | ( 0 << PD5 ) |
+			( 0 << PD6 ) | ( 0 << PD7 );
 
 	// 8-Bit таймер TC2
 	//Предделитель = 1024
 	TCCR2	 =	( 0 << WGM20 ) | ( 1 << WGM21) | ( 1 << CS22  ) |
-				( 1 << CS21  ) | ( 1 << CS20 );
+			( 1 << CS21  ) | ( 1 << CS20 );
 
 	//Compare Register, 3.9254 us timer freq = 12 Mhz
 	OCR2  = 0x2D;
@@ -116,20 +116,20 @@ if (bit_is_set(TIFR, OCF2))
 	gamepad2usb_data[0].buttons = 0;
 	
 	// Axes
-	if	(axis_up)		{ gamepad2usb_data[0].y = 0x81;}	//axis_up
-	if	(axis_down)		{ gamepad2usb_data[0].y = 0x7F;}	//axis_down
-	if	(axis_left)		{ gamepad2usb_data[0].x = 0x81;}	//axis_left
+	if	(axis_up)	{ gamepad2usb_data[0].y = 0x81;}	//axis_up
+	if	(axis_down)	{ gamepad2usb_data[0].y = 0x7F;}	//axis_down
+	if	(axis_left)	{ gamepad2usb_data[0].x = 0x81;}	//axis_left
 	if	(axis_right)	{ gamepad2usb_data[0].x = 0x7F;}	//axis_right
 
 	// Buttons
 	gamepad2usb_data[0].buttons =	(btn_0	<< 0) | // button 0
-									(btn_1	<< 1) | // button 1
-									(btn_2	<< 2) | // button 2
-									(btn_3	<< 3) | // button 3
-									(btn_4	<< 4) | // button 4
-									(btn_5	<< 5) | // button 5
-									(btn_6	<< 6) | // button 6
-									(btn_7	<< 7) ; // button 7
+					(btn_1	<< 1) | // button 1
+					(btn_2	<< 2) | // button 2
+					(btn_3	<< 3) | // button 3
+					(btn_4	<< 4) | // button 4
+					(btn_5	<< 5) | // button 5
+					(btn_6	<< 6) | // button 6
+					(btn_7	<< 7) ; // button 7
 
 	//Reset autofire mode if no buttons pressed
 	if ((gamepad2usb_data[0].buttons == 0)) { holdoncounter = 0; autofire_flag = 0; }
